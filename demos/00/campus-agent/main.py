1aefc5680b7208f672c98b3028ff5b6fcfa07c24
Simon Guest <me@simonguest.com>
2025-08-09 14:47:40 +0900

    Added second agent to demo

--- demos/00/campus-agent/main.py
+++ demos/00/campus-agent/main.py
@@ -3,7 +3,7 @@ from typing_extensions import TypedDict
 import gradio as gr
 from gradio import ChatMessage
 
-from agents import Agent, Runner, function_tool
+from agents import Agent, Runner, function_tool, FileSearchTool
 
 
 class Location(TypedDict):
@@ -21,10 +21,23 @@ def add_two_numbers(x: int, y: int) -> int:
     return x + y
 
 
+building_agent = Agent(
+    name="Building Agent",
+    instructions="You are a helpful building assistant. You can help students locate rooms on campus.",
+    tools=[
+        FileSearchTool(
+            max_num_results=3,
+            vector_store_ids=["vs_6896d8c959008191981d645850b42313"],
+            include_search_results=True,
+        )
+    ],
+)
+
 agent = Agent(
-    name="Planner",
-    instructions="You are a planner that can plan and execute tasks. Please be concise and accurate in distributing each question.",
-    tools=[fetch_weather],
+    name="Campus Assistant",
+    instructions="You are a helpful campus assistant that can plan and execute tasks for students. Please be concise and accurate in handing off tasks to other agents as needed.",
+    handoffs=[building_agent],
+    tools=[fetch_weather, add_two_numbers],
 )
 
 
@@ -44,15 +57,24 @@ async def chat_with_agent(user_msg: str, history: list):
             if event.type == "run_item_stream_event":
                 print(event.item)
                 if event.item.type == "tool_call_item":
-                    tool_name = getattr(event.item.raw_item, "name", "unknown_tool")
-                    tool_args = getattr(event.item.raw_item, "arguments", {})
-                    context["current_messages"].append(
-                        ChatMessage(
-                            role="assistant",
-                            content=f"Calling tool {tool_name} with arguments {tool_args}",
-                            metadata={"title": "Tool Call"},
+                    if event.item.raw_item.type == "file_search_call":
+                        context["current_messages"].append(
+                            ChatMessage(
+                                role="assistant",
+                                content="Searching the DigiPen vector store...",
+                                metadata={"title": "File Search"},
+                            )
                         )
-                    )
+                    else:
+                        tool_name = getattr(event.item.raw_item, "name", "unknown_tool")
+                        tool_args = getattr(event.item.raw_item, "arguments", {})
+                        context["current_messages"].append(
+                            ChatMessage(
+                                role="assistant",
+                                content=f"Calling tool {tool_name} with arguments {tool_args}",
+                                metadata={"title": "Tool Call"},
+                            )
+                        )
                 if event.item.type == "tool_call_output_item":
                     context["current_messages"].append(
                         ChatMessage(
@@ -68,15 +90,23 @@ async def chat_with_agent(user_msg: str, history: list):
                             content=event.item.raw_item.content[0].text,
                         )
                     )
+                if event.item.type == "handoff_call_item":
+                    context["current_messages"].append(
+                        ChatMessage(
+                            role="assistant",
+                            metadata={"title": "Handing Off Request"},
+                            content=f"Name: {event.item.raw_item.name}",
+                        )
+                    )
         yield context.get("current_messages", [])
 
 
 demo = gr.ChatInterface(
     chat_with_agent,
-    title="DigiPen Campus AI Agent",
+    title="DigiPen Campus Assistant",
     theme=gr.themes.Base(primary_hue="red", secondary_hue="slate"),
     examples=[
-        ["What's the weather in Seattle?"],
+        ["Where is the 'Hopper' room located?"],
     ],
     submit_btn=True,
     flagging_mode="manual",
